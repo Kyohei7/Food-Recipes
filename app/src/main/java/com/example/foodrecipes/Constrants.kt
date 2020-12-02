e888e7f357c57ad7624d3805187b796b03dcf552
Kyohei7 <rizkimuhammad2301@gmail.com>
2020-12-02 07:48:58 +0700

    Create Constrants for Url & Key Api & Fix package structure project

---
+++ app/src/main/java/com/example/foodrecipes/Constrants.kt
@@ -0,0 +1,12 @@
+package com.example.foodrecipes
+
+class Constrants {
+
+    companion object {
+
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "326d16da89554a8a801b7ed26bdf674a"
+
+    }
+
+}
